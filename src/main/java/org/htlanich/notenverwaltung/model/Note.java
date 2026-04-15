dd84cfd3cc8ba9959d372562ff2a0b94883c3a88
christophb <christophbecker733@gmail.com>
2026-04-15 09:10:15 +0200

    Note model gemacht

--- src/main/java/org/htlanich/notenverwaltung/model/Note.java
+++ src/main/java/org/htlanich/notenverwaltung/model/Note.java
@@ -1,5 +1,33 @@
 package org.htlanich.notenverwaltung.model;
+import jakarta.persistence.*;
 
-public class Note
-{
+@Entity
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int note;
+    private String fach;
+
+    public Note() {}
+
+    public Note(int note, String fach) {
+        this.note = note;
+        this.fach = fach;
+    }
+
+    public int getNote() {
+        return note;
+    }
+
+    public String getFach() {
+        return fach;
+    }
+
+    public void setNote(int note) {
+        this.note = note;
+    }
+
+    public void setFach(String fach) {
+        this.fach = fach;
+    }
 }
