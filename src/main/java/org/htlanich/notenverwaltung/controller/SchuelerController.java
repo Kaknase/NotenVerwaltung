2cbb81aa7a6f39a79e338252b4c0af68dbf111d0
Fabio <fabio08of@outlook.com>
2026-04-15 09:26:21 +0200

    -- Anzeigen von Schülern möglich (wir haben aber keine Daten)

--- src/main/java/org/htlanich/notenverwaltung/controller/SchuelerController.java
+++ src/main/java/org/htlanich/notenverwaltung/controller/SchuelerController.java
@@ -1,13 +1,13 @@
 package org.htlanich.notenverwaltung.controller;
 
-import org.htlanich.notenverwaltung.model.Note;
+import org.htlanich.notenverwaltung.model.Schueler;
 import org.htlanich.notenverwaltung.service.VerwaltungsService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("notenverwaltung/schueler")
@@ -15,8 +15,8 @@ public class SchuelerController
 {
     private VerwaltungsService verwaltungsService;
 
-    @GetMapping("anzeige/{id}")
-    public ArrayList<Note> getNoten(@PathVariable Long id){
+    @GetMapping("/anzeige/{id}")
+    public List<Schueler> getNoten(@PathVariable Long id){
         return verwaltungsService.noten(id);
     }
 }
--- src/main/java/org/htlanich/notenverwaltung/service/VerwaltungsService.java
+++ src/main/java/org/htlanich/notenverwaltung/service/VerwaltungsService.java
@@ -1,8 +1,31 @@
 package org.htlanich.notenverwaltung.service;
 
+import org.htlanich.notenverwaltung.model.Note;
+import org.htlanich.notenverwaltung.model.Schueler;
+import org.htlanich.notenverwaltung.model.schuelerRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class VerwaltungsService
 {
+
+    public final schuelerRepository schuelerRepository;
+
+    public VerwaltungsService(schuelerRepository schuelerRepository)
+    {
+        this.schuelerRepository = schuelerRepository;
+    }
+
+    List<Schueler> findBySchuelerIdContainingIgnoreCase(long id)
+    {
+        return schuelerRepository.findBySchuelerIdContainingIgnoreCase(id);
+    }
+
+
+    public List<Schueler> noten(Long id){
+        return findBySchuelerIdContainingIgnoreCase(id);
+    }
 }
